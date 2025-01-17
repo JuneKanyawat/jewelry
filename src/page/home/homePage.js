10534f58e7e4d09d0538ad4093fa02d38129c60d
Kanyawat Raksawin <kanyawat2002@gmail.com>
2025-01-17 20:03:19 +0700

    slideshow

--- src/page/home/homePage.js
+++ src/page/home/homePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "../../components/navbar/navbar.js";
+import SlideShow from "../../components/slideshow/slideshow.js";
 
 function HomePage() {
   return (
     <div>
       <NavBar />
+      <SlideShow />
     </div>
   );
 }
